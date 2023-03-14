67eb67af6b78ea195e20330047c414b3707b8075
Chock <chock@rosystain.com>
2023-03-14 14:20:53 +0800

    Update switch.sh
    Change Keyboard to K375s and Mouse to MX Master 2S ( the original code not work for me )

--- mac/switch.sh
+++ mac/switch.sh
@@ -3,7 +3,7 @@
 # KYB_CH = channel on unifying receiver for the destination keyboard. Typically maps to the 1 minus the key # you press to switch keyboards
 KYB_CH=0x00
 # KYB_ID = ID of the keyboard (typically this maps to the order of keyboard devices you see in the unifying software app in Advanced settings)
-KYB_ID=0x03
+KYB_ID=0x01
 
 # MSE_CH = channel on unifying receiver for the destination mouse. Typically maps 1 minus the channel button you press on the mouse to switch
 MSE_CH=0x00
@@ -17,10 +17,16 @@ RCVR_PID=C52B
 
 # Switch MX Keys to other device
 #                                                                                                                 A        B    C    D       E       F    G
-hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${KYB_ID},0x09,0x1e,${KYB_CH},0x00,0x00
+# hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${KYB_ID},0x09,0x1e,${KYB_CH},0x00,0x00
 # Switch MX Mouse to other device
 #                                                                                                                 A       B       C   D       E      F    G
-hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${MSE_ID},0x0a,0x1b,${MSE_CH},0x00,0x00
+# hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${MSE_ID},0x0a,0x1b,${MSE_CH},0x00,0x00
+# Switch K375s to other device
+#                                                                                                                 A        B    C    D       E       F    G
+hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${KYB_ID},0x08,0x1d,${KYB_CH},0x00,0x00
+# Switch MX Master 2S to other device
+#                                                                                                                 A       B       C   D       E      F    G
+hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${MSE_ID},0x09,0x1a,${MSE_CH},0x00,0x00
 
 exit 0
 
